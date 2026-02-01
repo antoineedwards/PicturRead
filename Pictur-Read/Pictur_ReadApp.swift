d792aeb4cbd3903d1c3dde70073dab8ac07b5bfc
antoineedwards <edwardsantoine333@gmail.com>
2026-02-01 16:14:06 -0600

    Initial Commit

---
+++ Pictur-Read/Pictur_ReadApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pictur_ReadApp.swift
+//  Pictur-Read
+//
+//  Created by Antoine Edwards on 2/1/26.
+//
+
+import SwiftUI
+
+@main
+struct Pictur_ReadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
